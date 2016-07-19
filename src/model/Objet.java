cf8db72ec134b9a45499004dba9dc71903935540
Spepsi <floriangardin@hotmail.fr>
2016-07-20 00:54:59 +0100

    refactor

--- src/model/Objet.java
+++ src/model/Objet.java
@@ -10,16 +10,19 @@ public abstract class Objet {
 	/*
 	 * Un objet est la classe de base dont hérite n'importe quel élément du monde
 	 * Il se compose d'un état (indexé par Index) ainsi qu'un vecteur d'actions possibles (Action)
+	 * Ainsi qu'une action courante (à méditer si une suffit)
 	 */
 	public float[] state;
 	public Vector<Action> actions;
-	// On stipule qu'on ne peut faire qu'une seule action à la fois , cad un seul état
-	public Action currentAction;
+	public Action currentAction;// On stipule qu'on ne peut faire qu'une seule action à la fois , cad un seul état, à réfléchir
 	
 	public void update(){
-		
+		// Comportement commun à tous les objets
+		for(Action a : actions){
+			
+		}
 	}
-	public abstract void render(Graphics g);
+	public abstract void render(Graphics g);//TODO :  La aussi idéalement lecomportementdevrait être commun, à méditer
 	
 	
 }
